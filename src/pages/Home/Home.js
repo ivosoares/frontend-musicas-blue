eeed1c9c2c8efa44ebd6dfcf9f2c18ec6be52283
Ivonaldo Soares <vonaldojrsoares@gmail.com>
2021-11-22 22:08:51 -0300

    troca o titulo da home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,7 @@ import ListMusic from '../../components/structure/ListMusic';
 const Home = () => {
   return (
     <div className="container">
-      <h1 className="text-center h1">PAGINA DA HOME</h1>
+      <h1 className="text-center h1">BLUE MUSICAS</h1>
       <ListMusic/>
     </div>
   )
